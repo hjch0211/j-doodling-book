f630932ded7b189a37ffe3fc52484f94579e1cf3
Jchan <gkswocks123@gmail.com>
2022-12-23 23:24:28 +0900

    Comment : Fail 페이지 todo 작성

--- src/pages/Fail/index.tsx
+++ src/pages/Fail/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+// [Todo] status와 message에 따라 보여주고 싶음
+// [?] 어떤 비즈니스를 여기로 오게 해야 하는게 맞을까
 function Fail() {
-  return <></>;
+  return <>Fail</>;
 }
 
 export default Fail;
