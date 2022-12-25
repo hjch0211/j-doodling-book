290f9f0cf9b2381f9e9f54b3428deba5d9974ca0
Jchan <gkswocks123@gmail.com>
2022-12-26 01:06:19 +0900

    Rename : colos -> colors

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { color } from "../components/contants/colors";
 // [Todo] common Page 컨테이너를 여기서 생성
 // 그 다음에 App.js에 추가해주기
 export const PageLayout = styled.div({
