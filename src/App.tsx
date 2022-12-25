1591e6b42a7d964de9c336d6475e378851cf7efc
Jchan <gkswocks123@gmail.com>
2022-12-26 01:48:05 +0900

    Feat : Create PageLayout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { Global, css } from "@emotion/react";
+import { Global as DefaultCssSetting, css } from "@emotion/react";
 import { MyRoutes } from "./pages/MyRoutes";
 import { PageLayout } from "./pages/PageLayout";
-import { color } from "./components/contants/colors";
 
 // [!] Provider는 여기에 위치시키려 함
 function App() {
   return (
     <Router>
-      <Global
+      <DefaultCssSetting
         styles={css`
           h1,
           h2,
@@ -20,9 +19,12 @@ function App() {
             font-weight: normal;
             margin: 0;
           }
-          body {
-            background-color: ${color.bgGrey};
-            color: ${color.textWhite};
+          html,
+          body,
+          #root {
+            margin: 0;
+            padding: 0;
+            height: 100%;
           }
         `}
       />
