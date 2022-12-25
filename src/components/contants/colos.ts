86f591b5b3ed61d24f768c124a6ff371cad32fe8
Jchan <gkswocks123@gmail.com>
2022-12-26 01:03:00 +0900

    Feat : Add Color.ts

---
+++ src/components/contants/colos.ts
@@ -0,0 +1,6 @@
+export const color = {
+  pastelYellow: "#F3EFE0",
+  turquoise: "#22A39F",
+  grey1: "#434242",
+  grey2: "#222222",
+};
---
+++ src/pages/PageLayout.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+// [Todo] common Page 컨테이너를 여기서 생성
+// 그 다음에 App.js에 추가해주기
+export const PageLayout = styled.div({
+  backgroundColor: "",
+});
