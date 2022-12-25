a8fdbcddfa1aec3c7201efbb0687f57f726eebea
Jchan <gkswocks123@gmail.com>
2022-12-26 01:27:54 +0900

    Feat : Add originColor obj

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { Global, css } from "@emotion/react";
 import { MyRoutes } from "./pages/MyRoutes";
+import { PageLayout } from "./pages/PageLayout";
+import { color } from "./components/contants/colors";
 
 // [!] Provider는 여기에 위치시키려 함
 function App() {
   return (
     <Router>
-      <MyRoutes />
+      <Global
+        styles={css`
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            font-size: 1rem;
+            font-weight: normal;
+            margin: 0;
+          }
+          body {
+            background-color: ${color.bgGrey};
+            color: ${color.textWhite};
+          }
+        `}
+      />
+      <PageLayout>
+        <MyRoutes />
+      </PageLayout>
     </Router>
   );
 }
