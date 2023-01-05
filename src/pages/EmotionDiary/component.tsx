eb46846e59195ad63d5b0bede6563df5fcbfcaf5
Jchan <gkswocks123@gmail.com>
2023-01-05 12:26:43 +0900

    Feat : emotionDiary

--- src/pages/EmotionDiary/component.tsx
+++ src/pages/EmotionDiary/component.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { Text } from "../../components/elements";
 import { RangeInput, RangeInputProps } from "../../components/elements";
