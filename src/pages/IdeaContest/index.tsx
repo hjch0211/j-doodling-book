6dcd92610b8004b51ce0dc425bf83906f4334bf4
Jchan <gkswocks123@gmail.com>
2023-01-12 10:09:49 +0900

    Delete

--- src/pages/IdeaContest/index.tsx
+++ src/pages/IdeaContest/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import MuiThemeProvider from "./MuiThemeProvider";
-import { Empty, H1 } from "../../components/elements";
 import { CommonLayout } from "../../components/CommonLayout";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 function IdeaContest() {
-  return <MuiThemeProvider></MuiThemeProvider>;
+  return (
+    <MuiThemeProvider>
+      <CommonLayout>
+        <Typography variant="h1">Innovators</Typography>
+        <Button variant="outlined" color="secondary">
+          확인
+        </Button>
+      </CommonLayout>
+    </MuiThemeProvider>
+  );
 }
 
 export default IdeaContest;
