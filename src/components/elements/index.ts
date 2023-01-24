94002b50ed378dbb7822ffea48430819bbb349cc
Jchan <gkswocks123@gmail.com>
2023-01-24 19:05:05 +0900

    🚚 Empty 파일 element -> atom - 아토믹 패턴 적용중

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -2,4 +2,4 @@ export { Text } from "./text/Text";
 export { H1, H2, H3 } from "./text/Head";
 export { RangeInput } from "./input/RangeInput";
 export type { RangeInputProps } from "./input/RangeInput";
-export { Empty } from "./Empty";
+export { Empty } from "../atom/Empty";
