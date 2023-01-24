014bf764c1419f607230c86a49270784dea653b0
Jchan <gkswocks123@gmail.com>
2023-01-24 22:38:16 +0900

    💡 Comment : 만능폼 제작 Todo 리스트 적음 - 유효성 검사 로직은 바닐라로 짜서 리액트 훅 폼을 대체할 수 있게 해보자

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -6,6 +6,7 @@ import { Empty } from "../../components/atom";
 export const Main = () => {
   const navigate = useNavigate();
   // [Todo] 화면에서 어떻게 보여줄까
+  // [Todo] 일단 헤더 무조건 필요
   return (
     <>
       <Empty height="5rem" />
---
+++ src/pages/VersatileForm/component/VersaInput.tsx
@@ -0,0 +1,6 @@
+import { Input } from "@mui/material";
+
+export const VersaInput = () => {
+  // [Todo] 단순 UI만을 나타낼 것
+  return <Input></Input>;
+};
